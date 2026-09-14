2d6c50793eeeef0dceff7509783940462d2da39f
MorastGit <morastsys@gmail.com>
2026-09-14 12:54:40 +0300

    Add Todo class

--- bin/lab2_buharev.dart
+++ bin/lab2_buharev.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:lab2_buharev/todo.dart';
+
+void main() {
+  Todo task1 = Todo(id: 1, title: 'Купить продукты');
+  Todo task2 = Todo(id: 2, title: 'Сделать зарядку');
+  task1.complete();
+  print(task1);
+  print(task2);
+}
---
+++ lib/todo.dart
@@ -0,0 +1,27 @@
+// class Todo {
+//   int? id;
+//   String? title;
+//   bool? isDone;
+// }
+
+// class Todo {
+//   int id = 0;
+//   String title = '';
+//   bool isDone = false;
+// }
+
+class Todo {
+  @override
+  late int id;
+  late String title;
+  late bool isDone;
+
+  Todo({required this.id, required this.title}) : isDone = false;
+  String toString() {
+    String status = isDone ? '[x]' : '[ ]';
+    return '$status $id. $title';
+  }
+  void complete(){
+    isDone = true;
+  }
+}
